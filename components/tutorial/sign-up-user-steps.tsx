c2dc6d4346dfc1fdebdb3149922f80c1e4130f59
Felicia <ffeliciachang@gmail.com>
2024-10-21 11:17:37 -0700

    change env var check

--- components/tutorial/sign-up-user-steps.tsx
+++ components/tutorial/sign-up-user-steps.tsx
@@ -5,18 +5,17 @@ import { ArrowUpRight } from "lucide-react";
 export default function SignUpUserSteps() {
   return (
     <ol className="flex flex-col gap-6">
-      {process.env.VERCEL_ENV === "preview" ||
-      process.env.VERCEL_ENV === "production" ? (
+      {process.env.NETLIFY ? (
         <TutorialStep title="Set up redirect urls">
-          <p>It looks like this App is hosted on Vercel.</p>
+          <p>It looks like this App is hosted on Netlify</p>
           <p className="mt-4">
             This particular deployment is
             <span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
-              "{process.env.VERCEL_ENV}"
+              "{process.env.BUILD_ID}"
             </span>{" "}
             on
             <span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
-              https://{process.env.VERCEL_URL}
+              https://{process.env.URL}
             </span>
             .
           </p>
@@ -42,13 +41,13 @@ export default function SignUpUserSteps() {
             <li>
               -{" "}
               <span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
-                {`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/**`}
+                {`https://${process.env.URL}/**`}
               </span>
             </li>
             <li>
               -{" "}
               <span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
-                {`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL?.replace(".vercel.app", "")}-*-[vercel-team-url].vercel.app/**`}
+                {`https://${process.env.URL?.replace(".vercel.app", "")}-*-[vercel-team-url].vercel.app/**`}
               </span>{" "}
               (Vercel Team URL can be found in{" "}
               <Link
