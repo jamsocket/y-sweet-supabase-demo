2a33c20b0c4c2503fa87f7fa5e9a1af82a7d2878
Felicia <ffeliciachang@gmail.com>
2024-11-05 08:23:47 -0800

    change env var to match supabase extension

--- utils/supabase/check-env-vars.ts
+++ utils/supabase/check-env-vars.ts
@@ -2,6 +2,6 @@
 // it is just for tutorial purposes
 
 export const hasEnvVars =
-  process.env.NEXT_PUBLIC_SUPABASE_URL &&
+  process.env.NEXT_PUBLIC_SUPABASE_DATABASE_URL &&
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY &&
   process.env.Y_SWEET_CONNECTION_STRING;
--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,6 +2,6 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
   createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_DATABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
